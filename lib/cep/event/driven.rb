7f965bd8916525a7a34380cc1e21a3032feb1905
Rodrigues <fr.costa95@gmail.com>
2021-02-07 17:35:55 -0300

    feat(init): initialize cep event driven

---
+++ lib/cep/event/driven.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "driven/version"
+
+module Cep
+  module Event
+    module Driven
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/cep/event/driven/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Cep
+  module Event
+    module Driven
+      VERSION = "0.1.0"
+    end
+  end
+end
